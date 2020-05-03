750dca45114a58037a88040842e502d9d48f58fe
rahul_p <rahulpyasi42@gmail.com>
2020-05-03 19:49:27 +0530

    Update Test.java

--- src/com/cyb/test/Test.java
+++ src/com/cyb/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	
 	
 public static void main(String[] args) {
-	System.out.println("This is first test");
+	System.out.println("This is first test changed from remote");
 	Student st = new Student("rahul");
 	System.out.println(st.getName());
 }
