81beaaaf06470a01bb04b4e93c85854c8c8d32ec
rahul pyasi <rahulpyasi42@gmail.com>
2020-05-03 20:47:42 +0530

    commit only one class to release

---
+++ src/com/cyb/test/TestRelease.java
@@ -0,0 +1,8 @@
+package com.cyb.test;
+
+public class TestRelease {
+	public static void main(String[] args) {
+	Student st = new Student("vasu release");
+		System.out.println(st.getName());
+	}
+}
