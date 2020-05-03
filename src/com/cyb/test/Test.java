67776fa775f245d31f4ed22172a007b766c0f8d5
rahul pyasi <rahulpyasi42@gmail.com>
2020-05-03 18:27:05 +0530

    First Commit

---
+++ src/com/cyb/test/Test.java
@@ -0,0 +1,12 @@
+package com.cyb.test;
+
+public class Test {
+	
+	
+public static void main(String[] args) {
+	System.out.println("This is first test");
+	Student st = new Student("rahul");
+	System.out.println(st.getName());
+}
+
+}
